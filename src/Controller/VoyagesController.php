5d4c7057d690900b019c5cadc580e17b9d39d24e
LaunMatt <launaymatthew15@gmail.com>
2025-09-30 19:56:39 +0200

    Phase 10 : ORM (affichage détaillé d'une visite)

--- src/Controller/VoyagesController.php
+++ src/Controller/VoyagesController.php
@@ -40,6 +40,14 @@ class VoyagesController extends AbstractController {
         ]);
     }
     
+    #[Route('/voyages/voyage/{id}', name: 'voyages.showone')]
+    public function showOne($id): Response{
+        $visite = $this->repository->find($id);
+        return $this->render("pages/voyage.html.twig", [
+            'visite' => $visite
+        ]);
+    }
+    
     /**
      * @var VisiteRepository
      */
